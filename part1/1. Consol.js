b6d8af9d04a9daaf41a7eb1a1644cd92ea33fa98
IGhost-P <79236624+IGhost-P@users.noreply.github.com>
2021-02-21 20:58:41 +0900

    part1

---
+++ part1/1. Consol.js
@@ -0,0 +1,42 @@
+
+크롬창을 여는 방법은
+1. 맥북 = option + cmd + i
+2. 윈도우 = ctr + shift + i 이다.
+
+    consol.log로 출력하기
+js에서 작성한 코드를 출력하는 명령어는
+'consol.log("해당 내용 이다.")'
+
+출력 예시
+1. consol.log("Hello world");
+2. consol.log('Hello world');
+3. consol.log(`Hello world`);
+가 있다.
+    wW
+링킹 하기
+
+이런 코드들을 작성해도, 작성하고자 하는 html 파일에 링킹되어 있지 않으면 html이 읽어낼수 없다.
+링킹 작업은 html 파일의 body 부분에
+    < script sc = 'js 형식의 파일 이름' > </script >
+        을 추가해주면 된다.
+
+데이터 타입
+
+1.Number
+2.String
+3.Boolean
+4.Undefind
+5.Null
+
+가 있다.
+js에서는 동적 타이핑이 지원이 되어서, 선언만 하면 데이터 타입이 결정이 된다.
+
+예를 들아
+let a = 3; let b = 'hi';
+를 선언을 하면, a의 는 Number로, b는 String로 정해진다.
+C 언어 처럼 int, char를 정할필요없다.
+
+대신 선언에 쓰이는 명령어가 있다.
+
+선언 명령어
+
