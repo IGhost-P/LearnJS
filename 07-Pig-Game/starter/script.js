cdb9cd1993f4c36725ec9d559e1341d99000797b
IGhost-P <79236624+IGhost-P@users.noreply.github.com>
2021-04-02 15:10:04 +0900

    만드는중 ㅠ

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -84,14 +84,17 @@ btnHold.addEventListener('click', function () {
         }
     }
 })
-
+// new 눌렀을때 원래대로 돌아오기
 btnNew.addEventListener('click', function () {
 
     playing = true;
-    document.querySelector(`.player--${activePlayer}`).classList.remove('player--winner');
-    // score = [0, 0];
-    // document.querySelector(`score--${activePlayer}`).textContent = 0;
-    // currentScore = 0;
+    document.querySelector(`.player--${activePlayer}`).classList.remove('.player--winner');
+    document.querySelector(`.player--${activePlayer}`).classList.remove('.player--active');
+    document.getElementById(`score--0`).textContent = 0;
+    document.getElementById(`score--1`).textContent = 0;
+    document.getElementById(`current--0`).textContent = 0;
+    document.getElementById(`current--1`).textContent = 0;
+    // // currentScore = 0;
     // activePlayer = 0;
 
 })
