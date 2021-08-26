fd8ed7ba9bf3e649fa685b1903d0a082d174d94f
IGhos-P <dndb3599@gmail.com>
2021-08-26 23:43:37 +0900

    파싱 연습

--- practice.js
+++ practice.js
@@ -1,13 +1,1 @@
-const obj = {
-    name: 'Lee',
-    age: 20,
-    alive: true,
-    hobby: ['traveling', 'tennis']
-}
-
-const json = JSON.stringify(obj);
-console.log(typeof json, json)
-
-const prettyJson = JSON.stringify(obj, null, 2)
-console.log(typeof prettyJson, prettyJson);
-``
+const foo = ()
