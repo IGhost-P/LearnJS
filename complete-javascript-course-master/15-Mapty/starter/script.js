40017e6b10e8350955a8dff85d8c00af6f1f45f4
IGhos-P <dndb3599@gmail.com>
2022-02-15 15:32:47 +0900

    아키텍처 적용전

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -10,3 +10,63 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map;
+let mapEvent;
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+    const map = L.map('map').setView([latitude, longitude], 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on(
+      'click',
+      function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputCadence.focus();
+      },
+      function () {
+        alert('Could not get your position');
+      }
+    );
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+
+      // Clear input fields
+      inputDistance.value =
+        inputCadence.value =
+        inputDuration.value =
+        inputElevation.value =
+          '';
+
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .setPopupContent('workout')
+        .openPopup();
+    });
+  });
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
