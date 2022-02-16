93495deaa1ab41a61ca0ac874b8dcc316cf79e74
IGhos-P <dndb3599@gmail.com>
2022-02-16 11:42:34 +0900

    완성

--- complete-javascript-course-master/15-Mapty/starter/script.js
+++ complete-javascript-course-master/15-Mapty/starter/script.js
@@ -15,13 +15,35 @@ class Workout {
   date = new Date();
   id = String(Date.now()).slice(-10); // 하지만 사용자 여러명이 동시에 사용할 가능성이 있기때문에 id를 시간으로 하는것은 좋지 않다
 
-  constructor(coords, distance, duraton) {
+  constructor(coords, distance, duration) {
     // 옛날엔..
     // this.date = new Date();
     // this. id = ... 이런식
     this.distance = distance;
-    this.duraton = duraton;
+    this.duration = duration;
     this.coords = coords;
+  }
+
+  _setDescription() {
+    // prettier-ignore => 아래 코드가 프리티어 되지 않음
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
   }
 }
 
@@ -31,6 +53,7 @@ class Running extends Workout {
     super(coords, distance, duration);
     this.cadence = cadence;
     this.calcPace();
+    this._setDescription(); // type이 없는 work class에서 는 사용하지 못한다
   }
 
   calcPace() {
@@ -41,10 +64,11 @@ class Running extends Workout {
 
 class Cycling extends Workout {
   type = 'cycling';
-  constructor(coords, distance, duration, elecationGain) {
+  constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
-    this.elecationGain = elecationGain;
+    this.elevationGain = elevationGain;
     this.calcSpeed();
+    this._setDescription();
   }
 
   calcSpeed() {
@@ -98,6 +122,19 @@ class App {
     this.#mapEvent = mapE;
     form.classList.remove('hidden');
     inputCadence.focus();
+  }
+
+  _hideForm() {
+    // Empty inputs
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+
+    form.style.display = 'none'; // 스타일을 먼저 없애고
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000); // 그 다음 스타일이 와야함
   }
 
   _toggleElevationField() {
@@ -155,18 +192,14 @@ class App {
     // Add new object to workout array
     this.#workouts.push(workout);
     // Render workout on map as marker
-    this.renderWorkoutMarker(workout); // this 키워드에 의해 나오기 때문에 bind를 안해줘도됨
+    this._renderWorkoutMarker(workout); // this 키워드에 의해 나오기 때문에 bind를 안해줘도됨
     // Render work on list
-
+    this._renderWorkout(workout);
     // Hide Clear inpur fields
-    inputDistance.value =
-      inputCadence.value =
-      inputDuration.value =
-      inputElevation.value =
-        '';
+    this._hideForm();
   }
 
-  renderWorkoutMarker(workout) {
+  _renderWorkoutMarker(workout) {
     L.marker(workout.coords)
       .addTo(this.#map)
       .bindPopup(
@@ -178,8 +211,61 @@ class App {
           className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent('workout')
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+      )
       .openPopup();
+  }
+
+  _renderWorkout(workout) {
+    let html = `
+      <li class="workout workout--${workout.type}" data-id="${workout.id}">
+        <h2 class="workout__title">${workout.description}</h2>
+        <div class="workout__details">
+          <span class="workout__icon">${
+            workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+          }</span>
+          <span class="workout__value">${workout.distance}</span>
+          <span class="workout__unit">km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⏱</span>
+          <span class="workout__value">${workout.duration}</span>
+          <span class="workout__unit">min</span>
+        </div>
+    `;
+
+    if (workout.type === 'running')
+      html += `
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.pace.toFixed(1)}</span>
+          <span class="workout__unit">min/km</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">🦶🏼</span>
+          <span class="workout__value">${workout.cadence}</span>
+          <span class="workout__unit">spm</span>
+        </div>
+      </li>
+      `;
+
+    if (workout.type === 'cycling')
+      html += `
+        <div class="workout__details">
+          <span class="workout__icon">⚡️</span>
+          <span class="workout__value">${workout.speed.toFixed(1)}</span>
+          <span class="workout__unit">km/h</span>
+        </div>
+        <div class="workout__details">
+          <span class="workout__icon">⛰</span>
+          <span class="workout__value">${workout.elevationGain}</span>
+          <span class="workout__unit">m</span>
+        </div>
+      </li>
+      `;
+    // 이래야 형제 요소 앞으로 추가가 된다
+    form.insertAdjacentHTML('afterend', html);
   }
 }
 
