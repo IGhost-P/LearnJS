7726f602476d423b613d032799c6b8a271ef87ec
IGhos-P <dndb3599@gmail.com>
2022-02-22 16:14:38 +0900

    17장들어감

---
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,6 @@
+// 기본 exrpot라면 {} degfault라면 안써도된다
+import { addToCart } from './shoppingCart.js';
+('./shoppingCart.js');
+console.log(`i am main script`);
+
+addToCart('bread', 5);
---
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -0,0 +1,10 @@
+console.log(`i am shopping Cart module`);
+// 비공개 변수가 된다(메인 모듈에서) // 기존 script였다면 전역변수가 되어서 다른 script에서도 사용할수 있었다
+const shoppingCost = 10;
+const cart = [];
+
+// 공개적으로 보내고 싶다면 exprot를 사용
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity}, ${product} added to cart`);
+};
