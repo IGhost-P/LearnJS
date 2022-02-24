4286d1729bbf4d6545b9c15aaf1168fcb43ac1ee
IGhos-P <dndb3599@gmail.com>
2022-02-24 23:10:54 +0900

    commit

--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,6 +1,33 @@
-// 기본 exrpot라면 {} degfault라면 안써도된다
-import { addToCart } from './shoppingCart.js';
-('./shoppingCart.js');
-console.log(`i am main script`);
+// // 기본 exrpot라면 {} degfault라면 안써도된다
+// // import { addToCart, totalPrice, totalQuantity } from './shoppingCart.js';
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 5);
+// add('apples', 4);
 
-addToCart('bread', 5);
+// console.log(cart);
+// Top-Level Await (ES2022)
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
+
+// const lastPost = getLastPost();
+// console.log(lastPost);
+
+// // Not very clean
+// lastPost.then(last => console.log(last));
+// console.log('not very clean');
+
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+// console.log('clean');
--- complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,10 +1,19 @@
 console.log(`i am shopping Cart module`);
 // 비공개 변수가 된다(메인 모듈에서) // 기존 script였다면 전역변수가 되어서 다른 script에서도 사용할수 있었다
 const shoppingCost = 10;
-const cart = [];
+export const cart = [];
 
 // 공개적으로 보내고 싶다면 exprot를 사용
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity}, ${product} added to cart`);
 };
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity}, ${product} added to cart`);
+}
+export { totalPrice, totalQuantity };
