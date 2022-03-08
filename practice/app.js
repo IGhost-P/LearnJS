2cbf0b2cc08a9d8ef0502eee2200d3296e84bf6e
IGhos-P <dndb3599@gmail.com>
2022-03-08 16:20:46 +0900

    init: practice

---
+++ practice/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set("views", "./views"); // 화면에 보여지는 부분을 처리함 (화면 뷰를 관리할 파일이 저장될 곳을 지정)
+app.set("view engine", "ejs"); // html코드를 어떤 엔진으로 해석할껀지? , ejs는 html과 거의 똑같은 엔진이다.
+
+app.get("/", (req, res) => {
+  res.render("home/index"); // views는 지정했기땨문
+});
+
+app.get("/login", (req, res) => {
+  res.render("home/login");
+});
+app.listen(3000, function () {
+  console.log("서버 가동");
+});
